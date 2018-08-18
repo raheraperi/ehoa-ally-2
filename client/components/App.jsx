4623d76caefc1a265441b2f0bec0927a88a24041
Rachael Peri <raheraperi@Rachaels-MacBook-Air.local>
2018-08-18 12:36:21 +1200

    adding node modules and confuguring client index.js

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {HashRouter as Router, Route, Switch} from 'react-router-dom'
+import {HashRouter as Router} from 'react-router-dom'
 
 import Nav from './Nav'
 class App extends React.Component {
