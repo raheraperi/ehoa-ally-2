3e18856ef6be5c8825ba27db2d4ad3dd4e04c592
Rachael Peri <raheraperi@Rachaels-MacBook-Air.local>
2018-08-18 16:50:34 +1200

    changes to main.css

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -2,16 +2,20 @@ import React from 'react'
 import {Route} from 'react-router-dom'
 
 import About from './About'
+import AllyOverview from './AllyOverview'
 
 const Home = () => {
   return (
-
-    <div className='columns'>
-
-      <div className='about column'>
-        <Route path='/' component={About} />
+    <div className='home-container'>
+      <div className='columns'>
+        <div className='about column'>
+          <Route path='/' component={About} />
+        </div>
       </div>
-
+      <hr />
+      <div className='ally column'>
+        <Route path='/' component={AllyOverview} />
+      </div>
     </div>
   )
 }
